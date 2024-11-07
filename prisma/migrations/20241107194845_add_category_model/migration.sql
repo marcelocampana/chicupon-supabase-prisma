7ff5e276f3f2cff16b204c7bf658946267519c5c
Marcelo Campaña <46510921+marcelocampana@users.noreply.github.com>
2024-11-07 21:47:22 +0000

    Add initial Supabase and Prisma setup with migrations, seed data, and configuration files

---
+++ prisma/migrations/20241107194845_add_category_model/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "Category" (
+    "category_id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "description" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("category_id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_name_key" ON "Category"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_slug_key" ON "Category"("slug");
